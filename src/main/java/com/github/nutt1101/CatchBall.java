6556cffcf4a12d2c83284c97acaa71a477462cfb
RICE0707 <master.end.9377@gmail.com>
2025-01-26 15:22:21 +0800

    移除更新檢查

--- src/main/java/com/github/nutt1101/CatchBall.java
+++ src/main/java/com/github/nutt1101/CatchBall.java
@@ -1,7 +1,6 @@
 package com.github.nutt1101;
 
 import java.util.logging.Level;
-
 import cn.handyplus.lib.adapter.HandySchedulerUtil;
 import com.github.nutt1101.command.Command;
 import com.github.nutt1101.command.TabComplete;
@@ -9,20 +8,15 @@ import com.github.nutt1101.event.DropGoldEgg;
 import com.github.nutt1101.event.GUIClick;
 import com.github.nutt1101.event.HitEvent;
 import com.github.nutt1101.event.SkullClick;
-import com.jeff_media.updatechecker.UpdateCheckSource;
-import com.jeff_media.updatechecker.UpdateChecker;
 import org.bukkit.ChatColor;
 import org.bukkit.command.PluginCommand;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
-import java.util.Arrays;
-import java.util.List;
 
-public class CatchBall extends JavaPlugin{
+public class CatchBall extends JavaPlugin {
     private FileConfiguration config = this.getConfig();
-
     public static Plugin plugin;
 
     private void checkPluginHook(String pluginName) {
@@ -33,20 +27,11 @@ public class CatchBall extends JavaPlugin{
 
     @Override
     public void onEnable() {
-
         plugin = this;
-
         ConfigSetting.checkConfig();
-
         new Metrics(this, 12380);
         registerEvent();
         registerCommand();
-
-        new UpdateChecker(this, UpdateCheckSource.HANGAR, "Maoyue_OUO/CatchBall/Release")
-                .checkEveryXHours(1) // Check every hour
-                .setDownloadLink("https://hangar.papermc.io/Maoyue_OUO/CatchBall/versions")
-                .setChangelogLink("https://hangar.papermc.io/Maoyue_OUO/CatchBall/versions")
-                .checkNow(); // And check right now
 
         checkPluginHook("Residence");
         checkPluginHook("MythicMobs");
@@ -55,14 +40,10 @@ public class CatchBall extends JavaPlugin{
         checkPluginHook("PlaceholderAPI");
         checkPluginHook("RedProtect");
         checkPluginHook("SimpleClaimSystem");
-        // TODO
-        // checkPluginHook("WorldGuard");
 
         HandySchedulerUtil.init(this);
-
     }
 
-    // register event
     public void registerEvent() {
         PluginManager registerEvent = this.getServer().getPluginManager();
         registerEvent.registerEvents(new HitEvent(), this);
@@ -71,7 +52,6 @@ public class CatchBall extends JavaPlugin{
         registerEvent.registerEvents(new GUIClick(), this);
     }
 
-    // register command
     public void registerCommand() {
         PluginCommand ctbCommand = this.getCommand("ctb");
         if (ctbCommand != null) {
@@ -83,5 +63,4 @@ public class CatchBall extends JavaPlugin{
     public static String getServerVersion() {
         return plugin.getServer().getBukkitVersion();
     }
-
 }
